c7f9054053047ec6a32d5b280984bb6775595f1e
blackair23 <deivid96iliev@gmail.com>
2022-12-06 00:40:20 +0200

    adding-homepage

---
+++ project/src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
+
+const apiURL = environment.apiURL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  loadTopBookings() {
+    return this.httpClient.get(`${apiURL}/data/catalog`);
+  }
+}
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+import { SharedModule } from './shared/shared.module';
+import { TopBookingsComponent } from './top-bookings/top-bookings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TopBookingsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
